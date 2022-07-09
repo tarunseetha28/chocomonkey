0c5068196bf3958a50bd17a85f8dbcfde4c8075d
tarunseetha28 <46785937+tarunseetha28@users.noreply.github.com>
2022-07-09 17:00:18 +0530

    mint page

--- src/component/Mint/Mint.js
+++ src/component/Mint/Mint.js
@@ -4,6 +4,7 @@ import Header from '../navbar/Navbar'
 import Footer from '../Contact/FooterContact'
 import gif1 from '../../assets/gif/gif1.gif'
 import MintSlider from './MintSlider'
+import BuyMint from './BuyMint'
 
 function Mint() {
     return (
@@ -15,8 +16,17 @@ function Mint() {
                     <span className='mint-h2'>MONKEYS</span>
                     <MintSlider />
                 </div>
-
+                <div className="mint-itr">
+                    <span style={{ color: "#63342E", fontSize: "40px", lineHeight: "40px", fontWeight: 'bold' }}>MINTING<br /> INSTRUCTIONS</span>
+                    <ul className='mint-list' style={{ marginTop: "30px" }}>
+                        <li>Connect your Wallet by clicking on the ‘CONNECT WALLET’ button and follow the instructions on MetaMask Note - Make sure your MetaMask Wallet is connected to the Ethereum Mainnet</li>
+                        <li>Choose the number of NFTs you want to mint, in the field below</li>
+                        <li>Click on the 'Buy Now button and the MetaMask window will pop up </li>
+                        <li>Click ‘Accept’ and then ‘Confirm’ on your Metamask wallet - Follow instructions until confirmation is shown</li>
+                    </ul>
+                </div>
             </div>
+            <BuyMint />
             <Footer />
         </>
     )
