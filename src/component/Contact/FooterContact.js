c129b4c3a68d0763ef67387daa7950865b0d9848
tarunseetha28 <46785937+tarunseetha28@users.noreply.github.com>
2022-07-13 14:41:46 +0530

    social icons

--- src/component/Contact/FooterContact.js
+++ src/component/Contact/FooterContact.js
@@ -4,6 +4,10 @@ import footericon from '../../assets/icons/footericon.svg'
 import discord from '../../assets/icons/discord.svg'
 import instagram from '../../assets/icons/instagram.svg'
 import twitter from '../../assets/icons/twitter.svg'
+import linkedin from '../../assets/icons/linkedinr.svg'
+import reddit from '../../assets/icons/redditr.svg'
+import telegram from '../../assets/icons/telegramr.svg'
+import medium from '../../assets/icons/mediumr.svg'
 
 function Footer() {
     return (
@@ -14,6 +18,10 @@ function Footer() {
                     <a href="https://discord.com/invite/MKYFkDh6yw" target="_blank"><img style={{ height: "2.5rem" }} src={discord} alt="" /></a>
                     <a href="https://twitter.com/ChocoMonkeys" target="_blank" ><img style={{ height: "2.5rem" }} src={twitter} alt="" /></a>
                     <a href="https://www.instagram.com/chocolatemonkeysnft5/" target="_blank"><img style={{ height: "2.5rem" }} src={instagram} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/chocolate-monkeys-nft-a375a3243/" target="_blank"><img style={{ height: "2.5rem" }} src={linkedin} alt="" /></a>
+                    <a href="https://www.reddit.com/user/ChocolateMonkeyNFT" target="_blank" ><img style={{ height: "2.5rem" }} src={reddit} alt="" /></a>
+                    <a href="https://t.me/chocolatemonkeysnft" target="_blank"><img style={{ height: "2.5rem" }} src={telegram} alt="" /></a>
+                    <a href="https://medium.com/@ChocolateMonkeysNFT" target="_blank"><img style={{ height: "2.5rem" }} src={medium} alt="" /></a>
                 </div>
                 <div className="footer-last-contact">
                     <span>Copyright © 2022 </span>
--- src/component/navbar/Hamburger.js
+++ src/component/navbar/Hamburger.js
@@ -3,6 +3,13 @@ import hamburgericon from "../../assets/icons/hamburgericon.svg"
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import 'antd/lib/drawer/style/index.css';
+import discord from '../../assets/icons/discord.svg'
+import instagram from '../../assets/icons/instagram.svg'
+import twitter from '../../assets/icons/twitter.svg'
+import linkedin from '../../assets/icons/linkedinr.svg'
+import reddit from '../../assets/icons/redditr.svg'
+import telegram from '../../assets/icons/telegramr.svg'
+import medium from '../../assets/icons/mediumr.svg'
 import { Drawer, Button } from 'antd'
 function Hamburger() {
     const [visible, setVisible] = useState(false);
@@ -38,6 +45,15 @@ function Hamburger() {
                     <Link className="navLink" aria-current="page" to="/news">News & articles</Link>
                     <Link className="navLink" aria-current="page" to="/media">Media</Link>
                     <Link className="navLink" aria-current="page" to="/press">Press Release</Link>
+                    <div className='nav-social'>
+                        <a href="https://discord.com/invite/MKYFkDh6yw" target="_blank"><img className='nav-img' src={discord} alt="" /></a>
+                        <a href="https://twitter.com/ChocoMonkeys" target="_blank" ><img className='nav-img' src={twitter} alt="" /></a>
+                        <a href="https://www.instagram.com/chocolatemonkeysnft5/" target="_blank"><img className='nav-img' src={instagram} alt="" /></a>
+                        <a href="https://www.linkedin.com/in/chocolate-monkeys-nft-a375a3243/" target="_blank"><img className='nav-img' src={linkedin} alt="" /></a>
+                        <a href="https://www.reddit.com/user/ChocolateMonkeyNFT" target="_blank" ><img className='nav-img' src={reddit} alt="" /></a>
+                        <a href="https://t.me/chocolatemonkeysnft" target="_blank"><img className='nav-img' src={telegram} alt="" /></a>
+                        <a href="https://medium.com/@ChocolateMonkeysNFT" target="_blank"><img className='nav-img' src={medium} alt="" /></a>
+                    </div>
                 </div>
 
             </Drawer>
